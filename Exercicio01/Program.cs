a413c01f001466b180b62751d4af11f1f7ded066
davidinovargas <davidinovargas@gmail.com>
2025-03-20 15:04:53 -0300

    realizado exercício02 e alterado código desnecessário no exercício01

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -10,10 +10,24 @@ namespace Exercicio01
             while (true)
             {
                 decimal comprimento = 0, altura = 0, largura = 0;
-                bool entradaValida = false;
-                Console.WriteLine("_________________________________________________________________________");
-                Console.WriteLine("Insira o comprimento, altura e largura da caixa (separados por vírgula): ");
-                Console.WriteLine("_________________________________________________________________________");
+                Console.Clear();
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("              _____________________");
+                Console.WriteLine("             /                    /|");
+                Console.WriteLine("            /                    / |");
+                Console.WriteLine("           /                    /  |");
+                Console.WriteLine("          /                    /   |");
+                Console.WriteLine("         /____________________/    |");
+                Console.WriteLine("         |                    |    |");
+                Console.WriteLine("         |                    |    |");
+                Console.WriteLine("         |                    |    /");
+                Console.WriteLine("         |                    |   /");
+                Console.WriteLine("         |                    |  /");
+                Console.WriteLine("         |                    | /");
+                Console.WriteLine("         |____________________|/");
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("Insira o comprimento, altura e largura da caixa: ");
+                Console.WriteLine("_________________________________________________\n");
                 string[] entrada = Console.ReadLine()!.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
                 if (entrada.Length == 3 &&
@@ -21,21 +35,17 @@ namespace Exercicio01
                     decimal.TryParse(entrada[1].Trim(), out altura) &&
                     decimal.TryParse(entrada[2].Trim(), out largura))
                 {
-                    entradaValida = true;
+                    decimal volume = comprimento * altura * largura;
+                    Console.WriteLine("_________________________________________________");
+                    Console.WriteLine($"        O volume da caixa é: {volume}cm³");
+                    Console.WriteLine("_________________________________________________");
                 }
-               
-                if (!entradaValida)
+                else
                 {
-                    Console.WriteLine("Entrada inválida!");
+                    Console.WriteLine("Entrada inválida! Aperte <ENTER> e tente novamente!");
+                    Console.ReadLine();
                     continue;
                 }
-
-                decimal volume = comprimento * altura * largura;
-                Console.WriteLine("_________________________________________________________________________");
-                Console.WriteLine($"                  O volume da caixa é: {volume}cm³");
-                Console.WriteLine("_________________________________________________________________________");
-
-
                 Console.Write("Deseja continuar? (s/N): ");
                 string opcaoContinuar = Console.ReadLine()!.ToUpper();
                 if (opcaoContinuar != "S")
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa para calcular o volume de um Cilindro
+            decimal raio = 0, altura = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("       Insira o raio e altura do cilindro: ");
+                Console.WriteLine("_________________________________________________\n");
+                string[] entrada = Console.ReadLine()!.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (entrada.Length == 2 &&
+                    decimal.TryParse(entrada[0].Trim(), out raio) &&
+                    decimal.TryParse(entrada[1].Trim(), out altura))
+                {
+                    decimal volume = (decimal)Math.PI * (decimal)Math.Pow((double)raio, 2) * altura;
+                    Console.WriteLine("_________________________________________________");
+                    Console.WriteLine($"      O volume do cilindro é: {volume:F2}cm³");
+                    Console.WriteLine("_________________________________________________");
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida! Aperte <ENTER> e tente novamente!");
+                    Console.ReadLine();
+                    continue;
+                }
+                Console.Write("Deseja continuar? (s/N): ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
