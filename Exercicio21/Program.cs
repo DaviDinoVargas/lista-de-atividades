3a62ca117f47a4f2d6a15108d1b62ed457a0da3d
davidinovargas <davidinovargas@gmail.com>
2025-03-23 23:57:06 -0300

    Finalizado Exercicio22

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercicio21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo de A! e o seu resultado.
+            while (true)
+            {
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("                FATORIAL N! ");
+                Console.WriteLine("_________________________________________________");
+
+                Console.Write("Digite um número inteiro positivo: ");
+                string entrada = Console.ReadLine()!;
+
+                if (int.TryParse(entrada, out int a) && a >= 0)
+                {
+                    long fatorial = 1;
+                    Console.Write($"{a}! = ");
+
+                    for (int i = a; i > 0; i--)
+                    {
+                        fatorial *= i;
+                        Console.Write(i + (i > 1 ? " x " : " = "));
+                    }
+
+                    Console.WriteLine(fatorial);
+                    Console.WriteLine("_________________________________________________\n");
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida!");
+                    continue;
+                }
+                Console.Write("Deseja continuar? (s/N): ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
---
+++ Exercicio22/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercicio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("_________________________________________________");
+                Console.WriteLine("         SEQUÊNCIA DE FIBONACCI ");
+                Console.WriteLine("_________________________________________________");
+
+                Console.Write("Digite um número inteiro positivo: ");
+                string entrada = Console.ReadLine()!;
+
+                if (int.TryParse(entrada, out int n) && n >= 0)
+                {
+                    int a = 0, b = 1, fibo;
+                    Console.WriteLine($"Sequência de Fibonacci até {n}:");
+
+                    Console.Write(a);
+                    if (n > 0) Console.Write($" {b}");
+
+                    while (b <= n)
+                    {
+                        fibo = a + b;
+                        if (fibo > n) break;
+                        Console.Write($" {fibo}");
+                        a = b;
+                        b = fibo;
+                    }
+
+                    Console.WriteLine("\n_________________________________________________\n");
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida!");
+                    continue;
+                }
+
+                Console.Write("Deseja continuar? (s/N): ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
+       
