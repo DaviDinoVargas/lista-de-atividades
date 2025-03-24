cdbeda048754d3dea61987c71bd263fd90bb87d7
davidinovargas <davidinovargas@gmail.com>
2025-03-23 23:58:40 -0300

    Finalizado Exercicio23

---
+++ Exercicio23/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine("                 FIZZBUZZ");
+            Console.WriteLine("_________________________________________________\n");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
