ead21326cc9b431fca165aaf780e1e4544d02066
davidinovargas <davidinovargas@gmail.com>
2025-03-23 23:31:54 -0300

    Finalizado Exercicio18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine("   Números ímpares entre 100 e 200:");
+            Console.WriteLine("_________________________________________________");
+
+            for (int i = 101; i < 200; i += 2)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine("\n_________________________________________________");
+            Console.ReadLine();
+        }
+    }
+}
